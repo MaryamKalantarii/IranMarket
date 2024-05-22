95b1e1f7b822aa92943d8a7096a9e3ba5dfa7304
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-05-22 18:50:44 +0330

    rest framwork

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'root',
+    'product',
+    'order',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
