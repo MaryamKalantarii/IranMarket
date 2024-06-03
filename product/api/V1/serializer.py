7b68345dc8ec1c257bf9fd480e4f7baa1a95e551
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-06-03 19:30:52 +0330

    to_representation

--- product/api/V1/serializer.py
+++ product/api/V1/serializer.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
-from ...models import *
+from product.models import Product,Category
 
 class ProductSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = Product
         fields = ["image","image1", "image2", "image3",
                     "title","content","content2","content3",
                     "content4","content5","price","number" ,
                     "warranty" ,"color" ,"category" ,"status" ]
-
+        
+    def to_representation(self,instance):
+        rep = super().to_representation(instance)
+        rep["category"] = CategorySerializer(instance.category,many=True).data
+        
 
 
 class CategorySerializer(serializers.ModelSerializer):
