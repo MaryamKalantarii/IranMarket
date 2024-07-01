8598d478417b7492cd4e2402bf3481be0d51f47b
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-07-01 13:46:13 +0330

    models

--- product/models.py
+++ product/models.py
@@ -55,6 +55,15 @@ class Category_brand(models.Model):
         return self.name
 
 
+class Category_color(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='colors')
+
+    def __str__(self):
+        return self.name
+
+
+
 class Clothing(models.Model):
     title = models.CharField(max_length=255)
     content1 = models.CharField(max_length=255)
@@ -70,6 +79,7 @@ class Clothing(models.Model):
 
     more_details = models.CharField(max_length=255)
     category_clothing = models.ManyToManyField(Category_clothing)
+    category_color = models.ManyToManyField(Category_color)
     price = models.IntegerField(default=0)
     number = models.IntegerField(default=0)
     off = models.IntegerField(null=True, blank=True)
@@ -113,6 +123,8 @@ class Dijitalgoods(models.Model):
     more_details = models.CharField(max_length=255)
     category_Dijitalgoods = models.ManyToManyField(Category_Dijitalgoods)
     category_brand = models.ManyToManyField(Category_brand)
+    category_color = models.ManyToManyField(Category_color)
+
 
     price = models.IntegerField(default=0)
     number = models.IntegerField(default=0)
@@ -147,13 +159,16 @@ class Homeappliances(models.Model):
     content4= models.CharField(max_length=255,null=True,blank=True)
     content5= models.CharField(max_length=255,null=True,blank=True)
 
-    image1 = models.ImageField(upload_to='dititalgoods')
-    image2 = models.ImageField(upload_to='dititalgoods', null=True ,blank=True)
-    image3 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
-    image4 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
+    image1 = models.ImageField(upload_to='homeappliances')
+    image2 = models.ImageField(upload_to='homeappliances', null=True ,blank=True)
+    image3 = models.ImageField(upload_to='homeappliances',null=True,blank=True)
+    image4 = models.ImageField(upload_to='homeappliances',null=True,blank=True)
     
     more_details = models.CharField(max_length=255)
     category_Dijitalgoods = models.ManyToManyField(Category_Dijitalgoods)
+    category_color = models.ManyToManyField(Category_color)
+    category_brand = models.ManyToManyField(Category_brand)
+
     price = models.IntegerField(default=0)
     number = models.IntegerField(default=0)
     off = models.IntegerField(null=True, blank=True)
@@ -191,13 +206,16 @@ class Beauty(models.Model):
     content4= models.CharField(max_length=255,null=True,blank=True)
     content5= models.CharField(max_length=255,null=True,blank=True)
 
-    image1 = models.ImageField(upload_to='dititalgoods')
-    image2 = models.ImageField(upload_to='dititalgoods', null=True ,blank=True)
-    image3 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
-    image4 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
+    image1 = models.ImageField(upload_to='beauty')
+    image2 = models.ImageField(upload_to='beauty', null=True ,blank=True)
+    image3 = models.ImageField(upload_to='beauty',null=True,blank=True)
+    image4 = models.ImageField(upload_to='beauty',null=True,blank=True)
     
     more_details = models.CharField(max_length=255)
     category_Beauty = models.ManyToManyField(Category_Beauty)
+    category_color = models.ManyToManyField(Category_color)
+    Category_brand = models.ManyToManyField(Category_brand)
+
     price = models.IntegerField(default=0)
     number = models.IntegerField(default=0)
     off = models.IntegerField(null=True, blank=True)
@@ -234,13 +252,16 @@ class Appliances(models.Model):
     content4= models.CharField(max_length=255,null=True,blank=True)
     content5= models.CharField(max_length=255,null=True,blank=True)
 
-    image1 = models.ImageField(upload_to='dititalgoods')
-    image2 = models.ImageField(upload_to='dititalgoods', null=True ,blank=True)
-    image3 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
-    image4 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
+    image1 = models.ImageField(upload_to='appliances')
+    image2 = models.ImageField(upload_to='appliances', null=True ,blank=True)
+    image3 = models.ImageField(upload_to='appliances',null=True,blank=True)
+    image4 = models.ImageField(upload_to='appliances',null=True,blank=True)
     
     more_details = models.CharField(max_length=255)
     category_Appliances = models.ManyToManyField(Category_Appliances)
+    category_color = models.ManyToManyField(Category_color)
+    Category_brand = models.ManyToManyField(Category_brand)
+
     price = models.IntegerField(default=0)
     number = models.IntegerField(default=0)
     off = models.IntegerField(null=True, blank=True)
@@ -275,10 +296,10 @@ class Supermarket(models.Model):
     content4= models.CharField(max_length=255,null=True,blank=True)
     content5= models.CharField(max_length=255,null=True,blank=True)
 
-    image1 = models.ImageField(upload_to='dititalgoods')
-    image2 = models.ImageField(upload_to='dititalgoods', null=True ,blank=True)
-    image3 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
-    image4 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
+    image1 = models.ImageField(upload_to='supermarkets')
+    image2 = models.ImageField(upload_to='supermarkets', null=True ,blank=True)
+    image3 = models.ImageField(upload_to='supermarkets',null=True,blank=True)
+    image4 = models.ImageField(upload_to='supermarkets',null=True,blank=True)
     
     more_details = models.CharField(max_length=255)
     category_Supermarket = models.ManyToManyField(Category_Supermarket)
@@ -317,14 +338,16 @@ class Child_and_baby(models.Model):
     content4= models.CharField(max_length=255,null=True,blank=True)
     content5= models.CharField(max_length=255,null=True,blank=True)
 
-    image1 = models.ImageField(upload_to='dititalgoods')
-    image2 = models.ImageField(upload_to='dititalgoods', null=True ,blank=True)
-    image3 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
-    image4 = models.ImageField(upload_to='dititalgoods',null=True,blank=True)
+    image1 = models.ImageField(upload_to='child_and_baby')
+    image2 = models.ImageField(upload_to='child_and_baby', null=True ,blank=True)
+    image3 = models.ImageField(upload_to='child_and_baby',null=True,blank=True)
+    image4 = models.ImageField(upload_to='child_and_baby',null=True,blank=True)
     
     more_details = models.CharField(max_length=255)
     category_Child_and_baby = models.ManyToManyField(Category_Child_and_baby)
     category_brand = models.ManyToManyField(Category_brand)
+    category_color = models.ManyToManyField(Category_color)
+    
     price = models.IntegerField(default=0)
     number = models.IntegerField(default=0)
     off = models.IntegerField(null=True, blank=True)
