473cc4ace9b2c5c5afed8d6783cbc72a448c1c82
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-07-13 14:46:54 +0330

    edit models accounts

--- accounts/models.py
+++ accounts/models.py
@@ -4,53 +4,45 @@ from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,Permissi
 # Create your models here.
 
 class CustomeBaseUserManager(BaseUserManager):
-    def create_user(self,email,phone,password, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('//')
-        
-        if phone.isnumeric==False:
-            raise ValueError('//')
-
+            raise ValueError("The given email must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, phone=phone)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, phone, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email,phone, password, **extra_fields)
-
-
-
-
-
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_verified") is not True:
+            raise ValueError("Superuser must have is_verified=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
 
 
-
-
-class CustomeUser(AbstractBaseUser,PermissionsMixin):
+class CustomeUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20 ,unique=True)
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=100, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    is_verifide = models.BooleanField(default=False)
-    create_date = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = "phone"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = CustomeBaseUserManager()
 
     def __str__(self):
         return self.email
+
 
