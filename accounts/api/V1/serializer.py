e9ef423a5982e23e30489de5704945557d6f3588
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-07-26 19:27:39 +0330

    ResetPassword

--- accounts/api/V1/serializer.py
+++ accounts/api/V1/serializer.py
@@ -112,3 +112,44 @@ class ProfailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profail
         fields = ["email", "first_name", "last_name", "image", "phone", "address"]
+
+
+
+class ResetPasswordEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(write_only=True)
+
+    def validate(self, attrs):
+        user = get_object_or_404(CustomeUser, email=attrs.get('email'))
+        attrs['user'] = user
+        return attrs
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+
+    new_password1 = serializers.CharField(max_length=20)
+    new_password2 = serializers.CharField(max_length=20)
+
+    def validate(self, attrs):
+        pass1 = attrs.get('new_password1')
+        pass2 = attrs.get('new_password2')
+
+        if pass1 != pass2:
+            raise serializers.ValidationError({'detail': 'Passwords do not match'})
+
+        return super().validate(attrs)
+
+    def set_new_password(self, request, attrs:dict):
+        pass1 = attrs.get("new_password1")
+        user = request.user
+
+        try:
+            validate_password(pass1)
+
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({"detail": list(e.messages)})
+        
+        user.set_password(pass1)
+        user.save()
+        return attrs
+    
+
