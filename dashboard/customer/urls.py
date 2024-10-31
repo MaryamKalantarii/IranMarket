7a79572fb02bfbe68b728110b1124afc6d421b20
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-10-31 23:45:14 +0330

    profile edit

--- dashboard/customer/urls.py
+++ dashboard/customer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("home/",CustomerDashboardHomeView.as_view(), name="home"),
     path("security-edit/",CustomerSecurityEditView.as_view(), name="security-edit"),
     path("profile/edit/",CustomerProfileEditView.as_view(),name="profile-edit"),
+    path("profile/image/edit/",CustomerProfileImageEditView.as_view(),name="profile-image-edit"),
 ]
--- dashboard/customer/views.py
+++ dashboard/customer/views.py
@@ -6,6 +6,9 @@ from django.contrib.messages.views import SuccessMessageMixin
 from dashboard.customer.forms import CustomerPasswordChangeForm,CustomerProfileEditForm
 from django.urls import reverse_lazy
 from accounts.models import Profile
+from django.shortcuts import redirect
+from django.contrib import messages
+
 class CustomerDashboardHomeView(LoginRequiredMixin, HasCustomerAccessPermission, TemplateView):
     template_name = "dashboard/customer/home.html"
 
@@ -23,3 +26,19 @@ class CustomerProfileEditView(LoginRequiredMixin, HasCustomerAccessPermission,Su
     
     def get_object(self, queryset=None):
         return Profile.objects.get(user=self.request.user)
+
+class CustomerProfileImageEditView(LoginRequiredMixin, HasCustomerAccessPermission,SuccessMessageMixin,UpdateView):
+    http_method_names=["post"]
+    model = Profile
+    fields= [
+        "image"
+    ]
+    success_url = reverse_lazy("dashboard:customer:profile-edit")
+    success_message = "بروز رسانی تصویر پروفایل با موفقیت انجام شد"
+    
+    def get_object(self, queryset=None):
+        return Profile.objects.get(user=self.request.user)
+    
+    def form_invalid(self, form):
+        messages.error(self.request,"ارسال تصویر با مشکل مواجه شده لطف مجدد بررسی و تلاش نمایید")
+        return redirect(self.success_url)
