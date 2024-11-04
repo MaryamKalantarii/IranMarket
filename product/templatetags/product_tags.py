a76a19abe7c401d04255b844e9b0a3d5f3f09d19
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-11-04 10:05:00 +0330

    similar other

--- product/templatetags/product_tags.py
+++ product/templatetags/product_tags.py
@@ -26,3 +26,21 @@ def show_similar_beauty(product):
     beauty_categories = product.category_Beauty.all()
     similar_beauty = Beauty.objects.filter(status=True, category_Beauty__in=beauty_categories).order_by('-created_date')[:6]
     return {"similar_beauty": similar_beauty}
+
+@register.inclusion_tag("includes/similar-app.html")
+def show_similar_app(product):
+    app_categories = product.category_Appliances.all()
+    similar_app = Appliances.objects.filter(status=True, category_Appliances__in=app_categories).order_by('-created_date')[:6]
+    return {"similar_app": similar_app}
+
+@register.inclusion_tag("includes/similar-supermarket.html")
+def show_similar_supermarket(product):
+    supermarket_categories = product.category_Supermarket.all()
+    similar_supermarket = Supermarket.objects.filter(status=True, category_Supermarket__in=supermarket_categories).order_by('-created_date')[:6]
+    return {"similar_supermarket": similar_supermarket}
+
+@register.inclusion_tag("includes/similar-child.html")
+def show_similar_child(product):
+    child_categories = product.category_Child_and_baby.all()
+    similar_child = Child_and_baby.objects.filter(status=True, category_Child_and_baby__in=child_categories).order_by('-created_date')[:6]
+    return {"similar_child": similar_child}
